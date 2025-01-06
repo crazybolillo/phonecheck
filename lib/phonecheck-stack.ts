0915d65e6296ab43598299c734e5d4fa32cfd5c4
CrazyBolillo <antonio@zoftko.com>
2025-01-05 23:20:35 -0600

    feat: create s3 bucket for recordings
    This is the s3 bucket where call recordings to be analyzed will be
    stored. A user with Read and Upload access to it was also created. So
    far it has been used with Twilio's external S3 recording functionality.

--- lib/phonecheck-stack.ts
+++ lib/phonecheck-stack.ts
@@ -1,16 +1,43 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
+import { aws_secretsmanager } from "aws-cdk-lib";
 
 export class PhonecheckStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+	constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+		super(scope, id, props);
 
-    // The code that defines your stack goes here
+		const recordingsBucket = new cdk.aws_s3.Bucket(
+			this,
+			"RecordingsBucket",
+			{
+				blockPublicAccess: cdk.aws_s3.BlockPublicAccess.BLOCK_ALL,
+				encryption: cdk.aws_s3.BucketEncryption.S3_MANAGED,
+				enforceSSL: true,
+			},
+		);
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'PhonecheckQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
-  }
+		const recordingsUser = new cdk.aws_iam.User(this, "RecordingsUser", {
+			userName: "recording-user-phonecheck",
+		});
+
+		recordingsBucket.grantPut(recordingsUser);
+		recordingsBucket.grantRead(recordingsUser);
+
+		const accessKey = new cdk.aws_iam.AccessKey(
+			this,
+			"RecordingUserAccessKey",
+			{
+				user: recordingsUser,
+			},
+		);
+
+		const recUserSecret = new aws_secretsmanager.Secret(
+			this,
+			"RecordingUserSecret",
+			{
+				description: "Access Key for uploading call recordings to S3",
+				secretStringValue: accessKey.secretAccessKey,
+			},
+		);
+	}
 }
